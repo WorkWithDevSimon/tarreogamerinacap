9c907a73c676bbe48b96e3158a6754910746b169
Simon <simonsantibanez16@gmail.com>
2024-10-14 14:07:27 -0300

    Termine el apartado de reglamentos

--- components/ComponenteReglamento/ComponenteRegla.jsx
+++ components/ComponenteReglamento/ComponenteRegla.jsx
@@ -1,15 +1,14 @@
 import "./ComponenteRegla.css"
-import { reglasTorneo } from './indexReglas'
+ 
 
-
-export const ComponenteRegla = () => {
+export const ComponenteRegla = ({reglasTorneo,SubituloReglas}) => {
 
     return (
         <>
             <section className="centrar-container">
 
                 <div className="reglas-container">
-                    <h2>Reglas del tarreo gamer</h2>
+                    <h2>{SubituloReglas}</h2>
                     <ul>
                         {reglasTorneo.map((regla, index) => (
                             <li key={index}>{regla}</li>
--- page/reglamento/Reglamento.jsx
+++ page/reglamento/Reglamento.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import "./Reglamento.css"
 import { ComponenteRegla } from '../../components/ComponenteReglamento/ComponenteRegla'
+import {
+    normasGenerales,
+    normasConvivencia,
+    reglasJuego,
+    normasSeguridad,
+    sanciones,
+    aceptacionReglamento
+} from './indexReglas'
 
 export const Reglamento = () => {
     return (
-        <ComponenteRegla></ComponenteRegla>
+        <>
+            <h1 className="Reglamento_title">Reglamento Tarreo Gamer Inacap Osorno 2024</h1>
+            <ComponenteRegla
+                reglasTorneo={normasGenerales} SubituloReglas={"Normas Generales"}></ComponenteRegla>
+            <ComponenteRegla
+                reglasTorneo={normasConvivencia} SubituloReglas={"Normas de Convivencia y Comportamiento"}></ComponenteRegla>
+            <ComponenteRegla
+                reglasTorneo={reglasJuego} SubituloReglas={"Reglas del Juego"}></ComponenteRegla>
+            <ComponenteRegla
+                reglasTorneo={normasSeguridad} SubituloReglas={"Normas de Seguridad"}></ComponenteRegla>
+            <ComponenteRegla
+                reglasTorneo={sanciones} SubituloReglas={"Sanciones"}></ComponenteRegla>
+            <ComponenteRegla
+                reglasTorneo={aceptacionReglamento} SubituloReglas={"Aceptación del Reglamento"}></ComponenteRegla>
+
+
+
+        </>
     )
 }
+
