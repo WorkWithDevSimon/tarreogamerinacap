5908ea17b02e341a5b789cc7b32b082435850a34
Simon <simonsantibanez16@gmail.com>
2024-10-15 00:02:14 -0300

    Aregle el apartado del boton del inicio

--- components/ComponenteIncio/ComponeteInicio.jsx
+++ components/ComponenteIncio/ComponeteInicio.jsx
@@ -7,8 +7,8 @@ export const ComponeteInicio = () => {
             <section className='div_inicio'>
                 <img src={DatosInicio.imgMain} alt="Fondo Tarreo" className="background-image" />
                 <h1 className='titulo'>{DatosInicio.titulo}</h1>
-                <div className='button_container'>
-                    <button className='boton'>
+                <div className='button_contenedor'>
+                    <button className='boton_Inicio'>
                         <a href={DatosInicio.botonUrl}>{DatosInicio.botonTexto}</a>
                     </button>
                 </div>
